e750bd5e8a7981ec73c934bc9fd3dd0e06adaebc
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-07-30 16:56:03 -0700

    seed data

---
+++ db/seed.sql
@@ -0,0 +1,29 @@
+INSERT INTO department(name)
+VALUES 
+('Management'),
+('Sales'),
+('Human Resources'),
+('Social Media'),
+('Office Staff'),
+('Engineers'),
+('Accounting');
+
+INSERT INTO roles(title, salary, department_id)
+VALUES
+('CEO', 100000, 1),
+('Sales Rep', 67000, 2),
+('HR Rep', 72000, 4),
+('Social Media Creator', 45000, 3),
+('Receptionist', 47000, 5),
+('Full Stack Dev', 150,000, 6)
+
+INSERT INTO employee(first_name, last_name, role_id) 
+VALUES
+('Master', 'Roshi', 1),
+('Android', '17', 5),
+('Android', '18', 2),
+('Goku', 'Son', 3),
+('Gohan', 'Son', 6),
+('Goten', 'Son', 3);
+
+UPDATE `employeeDB`.`employee` SET `manager_id` = '1' WHERE (`id` > '1');
--- index.js
+++
@@ -1,3 +0,0 @@
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const sequelize = require('./config/connection');
