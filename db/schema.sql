38396c213d4f189c8d2183d32ac153667268e34a
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-07-30 11:16:53 -0700

    finished schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,37 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
+DROP DATABASE IF EXISTS employeeDB;
+CREATE database employeeDB;
 
-USE employee_db;
+USE employeeDB;
+
+-- PARENT TABLE
+CREATE TABLE  department (
+did INTEGER(11) NOT NULL AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY (did)
+	);
 
 
+-- CHILD TABLE 
+CREATE TABLE roles (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL(9,2),
+  department_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  INDEX `idx_department_id`(department_id),
+  CONSTRAINT `fk_department_id`
+  FOREIGN KEY (department_id)
+  REFERENCES department(did) ON UPDATE CASCADE ON DELETE RESTRICT
+  );
+
+-- CHILD TABLE
 CREATE TABLE employee (
   id INTEGER(11) AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INTEGER NOT NULL,
   fkmanager_id INTEGER NOT NULL,
+  manager_id INTEGER NOT NULL,
   PRIMARY KEY (id),
   INDEX`idx_role`(role_id),
   CONSTRAINT `fk_role_id`
@@ -30,8 +52,14 @@ CREATE TABLE roles (
   salary DECIMAL(9,2),
   fkdepartment_id INTEGER NOT NULL,
   PRIMARY KEY (id),
-)
+);
+  REFERENCES roles(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+  INDEX `idx_manager`(manager_id), 
+  CONSTRAINT `fk_manager_id`
+  FOREIGN KEY (manager_id)
+  REFERENCES roles(id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
 
 SELECT * FROM employee;
 SELECT * FROM roles;
-SELECT * FROM department;
