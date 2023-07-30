2f25a67d571150966cb3b6861a61e1b2769b3e40
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-07-30 10:53:55 -0700

    working on schema for sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,36 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+
+CREATE TABLE employee (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INTEGER NOT NULL,
+  fkmanager_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  INDEX`idx_role`(role_id),
+  CONSTRAINT `fk_role_id`
+  FOREIGN KEY (role_id) 
+  REFERENCES department(did) ON UPDATE CASCADE ON DELETE RETRICT
+
+);
+
+CREATE TABLE  department (
+ did INTEGER(11) AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NULL,
+   PRIMARY KEY (did)  
+);
+
+CREATE TABLE roles (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL(9,2),
+  fkdepartment_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+)
+
+SELECT * FROM employee;
+SELECT * FROM roles;
+SELECT * FROM department;
